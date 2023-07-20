a8153393a5095bba0d2739cc5fa40fc2e183cfc2
Jaison Titus <jaisontj@amazon.com>
2023-07-20 14:04:46 +0000

    Added tmux conf and automatic vundle install

--- setup.sh
+++ setup.sh
@@ -1,19 +1,25 @@
-# Copy 
-cp .zshrc ${HOME}
-cp .vimrc ${HOME}
+# ZSH
+echo "Installing ohmyzsh"
+./install-ohmyzsh.sh
+echo "Setting gitprompt"
 cp .git-prompt.sh ${HOME} 
+echo "Setting up zshrc"
+cp .zshrc ${HOME}
+source ${HOME}/.zshrc
 
+# VIM
+echo "Setting up vim"
+cp .vimrc ${HOME}
 # Install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Run vundle install
+vim +PluginInstall +qall
 
 # Setup vim colors
 VIM_COLORS=${HOME}/.vim/colors
 mkdir -p ${VIM_COLORS}
 cp simple-dark.vim ${VIM_COLORS}
 
-# Install ohmyzsh
-./install-ohmyzsh.sh
-
-# Source
-source ${HOME}/.zshrc
-
+# TMUX
+echo "Install tmux based on your distro"
+cp .tmux.conf ${HOME}
